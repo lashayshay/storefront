08c8413f9897722c473997247f27d5cb015fbbea
James Shea <jshea.meche@gmail.com>
2022-09-14 17:22:07 +0300

    making migrations

--- likes/models.py
+++ likes/models.py
@@ -1,17 +1,13 @@
-from operator import mod
-from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 # Create your models here.
 
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
-
-class TaggedItem(models.Model):
-    # what tag applied to what object
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+class likedItem(models.Model):
+    # what user applied to what object
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     
     # two pieces of info:
     # what object type? product, article, video etc. use abstract model, installed app contenttype. django.contrib.contenttypes
